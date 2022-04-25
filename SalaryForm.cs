47a42a5453e58772747ef41ccf42b5f39a0a2614
kak7 <38856979+kaka0monstr@users.noreply.github.com>
2022-04-26 00:59:03 +0300

    113

--- SalaryForm.cs
+++ SalaryForm.cs
@@ -36,17 +36,65 @@ namespace ISAccounting
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-            
         }
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-            using (var reader = Program.database.GetReader($"select * from Salaries where name = '{comboBox1.Text}'"))
+            textBox1.Text = "";
+            using (var reader = Program.database.GetReader(
+                       $"select salary, premium from Salaries where name = '{comboBox1.Text}' and date = '{comboBox2.Text}'"))
             {
                 while (reader.Read())
                 {
-                    
+                    if (reader.IsDBNull(0))
+                    {
+                        Program.database.ExecuteNonQuery(
+                            $"insert into Salaries (name, salary, premium, date) values ('{comboBox1.Text}', null, null, '{comboBox2.Text}')");
+                        return;
+                    }
+
+                    textBox1.Text = reader.GetString(0);
+                    if (reader.IsDBNull(1)) return;
+                    textBox2.Text = reader.GetString(1);
                 }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "" | comboBox2.Text == "" | textBox1.Text == "")
+            {
+                button3.Enabled = false;
+                return;
+            }
+            else
+            {
+                button3.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Program.database.ExecuteNonQuery(
+                $"update Salaries set salary = '{textBox1.Text}' where name = '{comboBox1.Text}' and date = '{comboBox2.Text}'");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Program.database.ExecuteNonQuery(
+                $"update Salaries set premium = '{textBox2.Text}' where name = '{comboBox1.Text}' and date = '{comboBox2.Text}'");
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.Text == "" | comboBox2.Text == "" | textBox2.Text == "")
+            {
+                button2.Enabled = false;
+                return;
+            }
+            else
+            {
+                button2.Enabled = true;
             }
         }
     }
